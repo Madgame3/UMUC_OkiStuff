4858d176a5f8a73139808a1b5e1df6735dedfc32
Stephen Harrington <harrington.st@gmail.com>
2017-06-21 18:19:25 +0900

    header css

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
 <header>
 <script src='scripts/login.js'></script>
 <div><a href="./index.php"><img alt="brand" src="./images/logo1.png"></a></div>
-<div class="fb-login-button-Large" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" data-scope="public_profile,email,user_friends"></div>
-<div class="fb-login-button-Medium" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" data-scope="public_profile,email,user_friends"></div>
+<div>
+  <div class="fb-login-button-Large" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" data-scope="public_profile,email,user_friends"></div>
+  <div class="fb-login-button-Medium" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" data-scope="public_profile,email,user_friends"></div>
+</div>
 </header>
