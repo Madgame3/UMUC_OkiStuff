96c8aad569dc9253ebf54ae986fc28883716fad8
Stephen Harrington <harrington.st@gmail.com>
2017-06-21 17:07:30 +0900

    new fb script

--- header.php
+++ header.php
@@ -1,13 +1,6 @@
 <?php include_once("analyticstracking.php") ?>
 <header>
-  <a href="./index.php"><img alt="brand" src="./images/logo1.png"></a> 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=839841886165025";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script src='scripts/login.js'></script>
+<div><a href="./index.php"><img alt="brand" src="./images/logo1.png"></a></div>
 <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" data-scope="public_profile,email,user_friends"></div>
 </header>
